e83f50a029d2114f8e154fdce7799b68a75d6ba1
Andre Luiz <aribeiro.rj@gmail.com>
2026-09-21 15:26:37 -0300

    build: configura o alias @ para os imports

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./vitest.setup.ts'],
